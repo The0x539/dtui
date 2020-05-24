54c179c50d2cf82a312f018965e0bef2fabe0846
The0x539 <the0x539@gmail.com>
2020-05-24 13:04:25 -0500

    remove unused "border" view

--- src/views/mod.rs
+++ src/views/mod.rs
@@ -1,9 +1,7 @@
 mod torrents;
 mod filters;
 mod scroll;
-mod border;
 
 pub(crate) use torrents::TorrentsView;
 pub(crate) use filters::FiltersView;
 pub(crate) use scroll::ScrollInner;
-pub(crate) use border::*;
