04accec012daff9457bc37362c767bdeac1412c2
The0x539 <the0x539@gmail.com>
2020-06-03 16:20:39 -0500

    SpinView: expand the capabilities of the Spinnable trait

--- src/views/spin.rs
+++ src/views/spin.rs
@@ -18,18 +18,59 @@ use std::{
 };
 
 pub trait Spinnable: Default + PartialEq + PartialOrd + From<u8> + Copy + Display {
+    fn is_float() -> bool;
+
     fn checked_incr(self) -> Option<Self>;
     fn checked_decr(self) -> Option<Self>;
+
+    fn allows_negative(bounds: &impl RangeBounds<Self>) -> bool {
+        let zero = Self::from(0u8);
+
+        match bounds.start_bound() {
+            Bound::Excluded(min) | Bound::Included(min) => *min < zero,
+            Bound::Unbounded => zero.checked_decr().is_some(),
+        }
+    }
+
+    fn clamped_incr(self, bounds: &impl RangeBounds<Self>) -> Self {
+        match (self.checked_incr(), bounds.end_bound()) {
+            // If incrementing overflows, but the end bound is inclusive, use that.
+            (None, Bound::Included(max)) => *max,
+
+            // Otherwise, if incrementing doesn't overflow and the bounds allow it, use that.
+            (Some(v), _) if bounds.contains(&v) => v,
+
+            // Otherwise, we can't safely increment without overflowing or exceeding the bounds.
+            // (3.5f64).clamped_incr(..4.0f64) == 3.5f64
+            _ => self,
+        }
+    }
+
+    fn clamped_decr(self, bounds: &impl RangeBounds<Self>) -> Self {
+        // See above
+        match (self.checked_decr(), bounds.start_bound()) {
+            (None, Bound::Included(min)) => *min,
+            (Some(v), _) if bounds.contains(&v) => v,
+            _ => self,
+        }
+    }
 }
+
 impl Spinnable for u64 {
+    fn is_float() -> bool { false }
     fn checked_incr(self) -> Option<Self> { self.checked_add(1) }
     fn checked_decr(self) -> Option<Self> { self.checked_sub(1) }
+    fn allows_negative(_: &impl RangeBounds<Self>) -> bool { false }
 }
+
 impl Spinnable for i64 {
+    fn is_float() -> bool { false }
     fn checked_incr(self) -> Option<Self> { self.checked_add(1) }
     fn checked_decr(self) -> Option<Self> { self.checked_sub(1) }
 }
+
 impl Spinnable for f64 {
+    fn is_float() -> bool { true }
     fn checked_incr(self) -> Option<Self> {
         Some(self + 1.0).filter(|x| x.is_finite())
     }
@@ -82,35 +123,20 @@ impl<T: Spinnable, B: RangeBounds<T>> SpinView<T, B> where Self: 'static {
         Self { bounds, val, edit_id, panel }.with_name(id)
     }
 
-    fn set(&mut self) -> Callback {
-        let val_str = self.val.to_string();
+    fn set_val(&mut self, new_val: T) -> Callback {
+        let val_str = new_val.to_string();
+        self.val = new_val;
         self.panel
             .call_on_name(&self.edit_id, |v: &mut EditView| v.set_content(val_str))
             .unwrap()
     }
 
     fn decr(&mut self) -> Callback {
-        self.val = match self.val.checked_decr() {
-            Some(v) if self.bounds.contains(&v) => v,
-            _ => if let Bound::Included(min) = self.bounds.start_bound() {
-                *min
-            } else {
-                self.val
-            },
-        };
-        self.set()
+        self.set_val(self.val.clamped_decr(&self.bounds))
     }
 
     fn incr(&mut self) -> Callback {
-        self.val = match self.val.checked_incr() {
-            Some(v) if self.bounds.contains(&v) => v,
-            _ => if let Bound::Included(max) = self.bounds.end_bound() {
-                *max
-            } else {
-                self.val
-            },
-        };
-        self.set()
+        self.set_val(self.val.clamped_incr(&self.bounds))
     }
 }
 
@@ -118,7 +144,6 @@ impl<T: Spinnable, B: RangeBounds<T>> ViewWrapper for SpinView<T, B>
 where Self: 'static {
     cursive::wrap_impl!(self.panel: Panel<LinearLayout>);
 
-    /*
     fn wrap_required_size(&mut self, mut constraint: Vec2) -> Vec2 {
         constraint.y = 3; // no tallness allowed
         self.panel.required_size(constraint)
@@ -131,5 +156,4 @@ where Self: 'static {
         //                                      │ + │
         printer.print((printer.size.x - 9, 2), "┴───┴");
     }
-    */
 }
