2826ee3ca8dfb98b5b89977634ddcd231f18489b
The0x539 <the0x539@gmail.com>
2020-06-17 11:51:48 -0500

    config: function for saving

--- src/config.rs
+++ src/config.rs
@@ -5,6 +5,8 @@ use serde::{Serialize, Deserialize};
 use lazy_static::lazy_static;
 
 type FnvIndexMap<K, V> = indexmap::IndexMap<K, V, fnv::FnvBuildHasher>;
+
+const APP_NAME: &str = "dtui";
  
 #[derive(Serialize, Deserialize)]
 pub struct Host {
@@ -26,9 +28,18 @@ pub struct Config {
     pub connection_manager: ConnectionManagerConfig,
 }
 
+impl Config {
+    fn save(&mut self) {
+        // Mutation isn't required, but exclusive access makes sense.
+        // Moreover, if you didn't already have a mutable ref to the config,
+        // then you can't possibly have any changes to save anyway.
+        confy::store(APP_NAME, self).unwrap()
+    }
+}
+
 lazy_static! {
     static ref CONFIG: Arc<RwLock<Config>> = {
-        let cfg: Config = confy::load("dtui").unwrap();
+        let cfg: Config = confy::load(APP_NAME).unwrap();
         let cmgr = &cfg.connection_manager;
         if let Some(id) = cmgr.autoconnect {
             assert!(cmgr.hosts.contains_key(&id));
