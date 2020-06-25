5bd477fa9927c591c97fcffcd9fbd93550c66bc2
The0x539 <the0x539@gmail.com>
2020-06-25 14:29:31 -0500

    Form: add custom_dismiss constructor

--- src/form.rs
+++ src/form.rs
@@ -4,6 +4,36 @@ use cursive::view::{View, ViewWrapper};
 use cursive::views::{Dialog, EditView, ResizedView, TextArea};
 use cursive::Cursive;
 
+fn make_cb<T, F>(f: F) -> impl Fn(&mut Cursive)
+where
+    T: Form,
+    F: FnOnce(&mut Cursive, T::Data),
+{
+    let mut f = Some(f);
+    let cb = move |siv: &mut Cursive| {
+        let f = match f.take() {
+            Some(f) => f,
+            None => return,
+        };
+
+        let dialog: Box<Dialog> = siv
+            .pop_layer()
+            .expect("no layer")
+            .downcast::<Dialog>()
+            .ok()
+            .expect("top layer wasn't a Dialog");
+
+        let form: Box<T> = dialog
+            .into_content()
+            .downcast::<T>()
+            .ok()
+            .expect("dialog's contents weren't Self");
+
+        f(siv, form.into_data());
+    };
+    cursive::immut1!(cb)
+}
+
 pub trait Form: View + Sized + 'static {
     type Data;
 
@@ -15,34 +45,21 @@ pub trait Form: View + Sized + 'static {
         submit_label: impl Into<String>,
         on_submit: impl FnOnce(&mut Cursive, Self::Data) + 'static,
     ) -> Dialog {
-        let mut f = Some(on_submit);
-        let cb = move |siv: &mut Cursive| {
-            let on_submit = match f.take() {
-                Some(f) => f,
-                None => return,
-            };
-
-            let dialog: Box<Dialog> = siv
-                .pop_layer()
-                .expect("no layer")
-                .downcast::<Dialog>()
-                .ok()
-                .expect("top layer wasn't a Dialog");
-
-            let form: Box<Self> = dialog
-                .into_content()
-                .downcast::<Self>()
-                .ok()
-                .expect("dialog's contents weren't Self");
-
-            let data = form.into_data();
-
-            on_submit(siv, data);
-        };
-
         Dialog::around(self)
-            .button(submit_label, cursive::immut1!(cb))
+            .button(submit_label, make_cb::<Self, _>(on_submit))
             .dismiss_button(dismiss_label)
+    }
+
+    fn into_dialog_custom_dismiss(
+        self,
+        dismiss_label: impl Into<String>,
+        submit_label: impl Into<String>,
+        on_submit: impl FnOnce(&mut Cursive, Self::Data) + 'static,
+        on_dismiss: impl FnOnce(&mut Cursive, Self::Data) + 'static,
+    ) -> Dialog {
+        Dialog::around(self)
+            .button(submit_label, make_cb::<Self, _>(on_submit))
+            .button(dismiss_label, make_cb::<Self, _>(on_dismiss))
     }
 }
 
