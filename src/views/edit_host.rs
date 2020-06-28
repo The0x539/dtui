ae0968425f7fe9fa78cc58d0be71e89e21fc0387
The0x539 <the0x539@gmail.com>
2020-06-28 13:39:40 -0500

    EditHostView: use TextAreas, not TextViews

--- src/views/edit_host.rs
+++ src/views/edit_host.rs
@@ -4,7 +4,7 @@ use crate::form::Form;
 use crate::views::{linear_panel::LinearPanel, spin::SpinView};
 
 use cursive::view::ViewWrapper;
-use cursive::views::{EditView, LinearLayout, TextView};
+use cursive::views::{LinearLayout, TextArea, TextView};
 
 type PortSpinView = SpinView<u16, std::ops::RangeFull>;
 
@@ -16,16 +16,16 @@ impl EditHostView {
     pub fn new(hostname: &str, port: u16, username: &str, password: &str) -> Self {
         let host_row = LinearLayout::horizontal()
             .child(TextView::new("Hostname: "))
-            .child(EditView::new().content(hostname))
+            .child(TextArea::new().content(hostname))
             .child(SpinView::new(Some("Port: "), None, ..).with_val(port));
 
         let username_row = LinearLayout::horizontal()
             .child(TextView::new("Username: "))
-            .child(EditView::new().content(username));
+            .child(TextArea::new().content(username));
 
         let password_row = LinearLayout::horizontal()
             .child(TextView::new("Password: "))
-            .child(EditView::new().content(password));
+            .child(TextArea::new().content(password));
 
         let inner = LinearPanel::vertical()
             .child(host_row, None)
@@ -79,10 +79,10 @@ impl Form for EditHostView {
         let mut username_row = take_row(&mut inner, 1);
         let mut host_row = take_row(&mut inner, 0);
 
-        let username: String = take_content::<EditView>(&mut username_row, 1);
-        let password: String = take_content::<EditView>(&mut password_row, 1);
+        let username: String = take_content::<TextArea>(&mut username_row, 1);
+        let password: String = take_content::<TextArea>(&mut password_row, 1);
         let port: u16 = take_content::<PortSpinView>(&mut host_row, 2);
-        let address: String = take_content::<EditView>(&mut host_row, 1);
+        let address: String = take_content::<TextArea>(&mut host_row, 1);
 
         Host {
             address,
